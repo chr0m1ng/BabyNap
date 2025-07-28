e0cbb71efdfcb9e344057260cf0ee35a9b8271b6
Gabriel Santos <gabrielrsantoss@icloud.com>
2025-07-28 17:45:03 -0300

    initial project

--- BabyNap/BabyNapApp.swift
+++ BabyNap/BabyNapApp.swift
@@ -14,7 +14,7 @@ struct BabyNapApp: App {
         let schema = Schema([
             Item.self,
         ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false, cloudKitDatabase: .none)
 
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
